8097ee71e386c9abe859da07206d9d7f892e9a3a
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-07-27 21:21:49 +0700

    add products_list, products_list.html, inc_menu.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Product
+
 
 def home(request):
     return render(request, 'home.html')
@@ -16,5 +18,7 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 
-def index(request):
-    return render(request, 'base.html')
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products_list.html', context)
