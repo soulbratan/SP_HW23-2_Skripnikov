b8d427c410d1c092df9044992ef257e9ced01cbc
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-08-17 08:13:42 +0700

    Correction of comments

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views import View
 from catalog.models import Product
 
@@ -20,41 +20,15 @@ class ContactsView(View):
         )
 
 
-class HomeView(View):
+class HomeView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, 'catalog/home.html')
-
-
-# def home(request):
-#     return render(request, 'home.html')
-
-# def contacts(request):
-#     if request.method == 'POST':
-#         # Получение данных из формы
-#         name = request.POST.get('name')
-#         message = request.POST.get('message')
-#         phone = request.POST.get('phone')
-#         # Обработка данных (например, сохранение в БД, отправка email и т. д.)
-#         # Здесь мы просто возвращаем простой ответ
-#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено. Сообщение: <{message}>. Телефон: <{phone}>")
-#     return render(request, 'catalog/contacts.html')
 
 
 class ProductListView(ListView):
     model = Product
 
 
-# def product_list(request):
-#     products = Product.objects.all()
-#     context = {'products': products}
-#     return render(request, 'products_list.html', context)
-
-
 class ProductDetailView(DetailView):
     model = Product
 
-
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {'product': product}
-#     return render(request, 'product_detail.html', context)
