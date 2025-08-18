31106fb03825235703edb17741cd8c11ff9c6286
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-08-18 15:18:25 +0700

    validates for 'name' and 'description'

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,8 +1,13 @@
 from django.db.models import BooleanField
 from django.forms import ModelForm
-
+from django.core.exceptions import ValidationError
 from catalog.models import Product
 
+
+BANNED_WORDS = [
+    "казино", "криптовалюта", "крипта", "биржа",
+    "дешево", "бесплатно", "обман", "полиция", "радар"
+]
 
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
@@ -18,4 +23,18 @@ class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         exclude = ("views_count",)
+
+    def clean_name(self):
+        name = self.cleaned_data['name'].lower()
+        for word in BANNED_WORDS:
+            if word in name:
+                raise ValidationError(f"Запрещено использовать слово: '{word}'!")
+        return self.cleaned_data['name']
+
+    def clean_description(self):
+        description = self.cleaned_data['description'].lower()
+        for word in BANNED_WORDS:
+            if word in description:
+                raise ValidationError(f"Запрещено использовать слово: '{word}'!")
+        return self.cleaned_data['description']
 
